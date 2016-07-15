ce4189426f976f901a62ffda3c7d356c44331446
Matt Kaemmerer <mattk@spiceworks.com>
2016-07-15 17:21:03 -0500

    Build package using webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: './dist',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};

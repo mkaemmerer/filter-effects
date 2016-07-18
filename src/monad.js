142f7d5d4f732f06a081b35a54e0410ec8c60f28
Matt Kaemmerer <mattk@spiceworks.com>
2016-07-17 21:23:48 -0500

    Fix monad.do reuse problem

--- src/monad.js
+++ src/monad.js
@@ -1,11 +1,21 @@
 import Free from './free';
 
+const cached = f => {
+  let value = undefined;
+  return x => {
+    if(value === undefined){
+      value = f(x);
+    }
+    return value;
+  };
+};
+
 const Monad = {
   do: gen => {
     const g    = gen();
     const step = data => {
       const {done, value} = g.next(data);
-      return done ? value : value.flatMap(step);
+      return done ? value : value.flatMap(cached(step));
     };
     return step();
   },

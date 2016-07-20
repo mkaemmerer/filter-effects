0437e92e3afa9f7e73811a0fd34d8b0e59eb7333
Matt Kaemmerer <mattk@spiceworks.com>
2016-07-19 20:40:10 -0500

    Add example of using debug

---
+++ examples/debug/src/index.js
@@ -0,0 +1,71 @@
+import { Monad, run, create }  from '../../../index';
+import * as n from '../../../index';
+import build from '../../../src/extras/debug';
+
+
+const crossFade = (x, y, amt) => Monad.do(function *() {
+  const attenuated = yield n.feColorMatrix({
+    in: x,
+    type: 'matrix',
+    values: `1 0 0  0     0
+             0 1 0  0     0
+             0 0 1  0     0
+             0 0 0 ${amt} 0`
+  });
+  const blend      = yield n.feBlend({
+    in:  attenuated,
+    in2: y,
+    mode: 'normal'
+  });
+
+  return Monad.of(blend);
+});
+
+const dropShadow = source => Monad.do(function *() {
+  const inverse    = yield n.feColorMatrix({
+    in: source,
+    type: 'matrix',
+    values: `1 0 0  0 0
+             0 1 0  0 0
+             0 0 1  0 0
+             0 0 0 -1 1`
+  });
+  const shadow     = yield n.feGaussianBlur({
+    in: inverse,
+    stdDeviation: 2
+  });
+  const shifted    = yield n.feOffset({
+    in: shadow,
+    dy: 5
+  });
+  const clipped    = yield n.feComposite({
+    in:  shifted,
+    in2: source,
+    operator: 'in'
+  });
+  const full       = yield n.feBlend({
+    in:  source,
+    in2: clipped,
+    mode: 'multiply'
+  });
+  const blend      = yield crossFade(source, full, 0.7);
+
+  return Monad.of(blend);
+});
+
+const program = Monad.do(function *() {
+  const source = yield n.sourceGraphic();
+  const shadow = yield dropShadow(source);
+  return Monad.of(shadow);
+});
+
+const filterAttrs = {
+  id:     'inner-shadow',
+  x0:     '-50%',
+  y0:     '-50%',
+  width:  '200%',
+  height: '200%'
+};
+
+const svg = build(run(program), filterAttrs);
+document.body.appendChild(svg);

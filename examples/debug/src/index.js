6ee04a89f39252015178c7b7556642243170bc78
Matt Kaemmerer <mattk@spiceworks.com>
2016-07-20 11:08:31 -0500

    Rename filter effects import to f

--- examples/debug/src/index.js
+++ examples/debug/src/index.js
@@ -1,10 +1,10 @@
 import { Monad, run, create }  from '../../../index';
-import * as n from '../../../index';
+import * as f from '../../../index';
 import build from '../../../src/extras/debug';
 
 
 const crossFade = (x, y, amt) => Monad.do(function *() {
-  const attenuated = yield n.feColorMatrix({
+  const attenuated = yield f.feColorMatrix({
     in: x,
     type: 'matrix',
     values: `1 0 0  0     0
@@ -12,7 +12,7 @@ const crossFade = (x, y, amt) => Monad.do(function *() {
              0 0 1  0     0
              0 0 0 ${amt} 0`
   });
-  const blend      = yield n.feBlend({
+  const blend      = yield f.feBlend({
     in:  attenuated,
     in2: y,
     mode: 'normal'
@@ -22,7 +22,7 @@ const crossFade = (x, y, amt) => Monad.do(function *() {
 });
 
 const dropShadow = source => Monad.do(function *() {
-  const inverse    = yield n.feColorMatrix({
+  const inverse    = yield f.feColorMatrix({
     in: source,
     type: 'matrix',
     values: `1 0 0  0 0
@@ -30,20 +30,20 @@ const dropShadow = source => Monad.do(function *() {
              0 0 1  0 0
              0 0 0 -1 1`
   });
-  const shadow     = yield n.feGaussianBlur({
+  const shadow     = yield f.feGaussianBlur({
     in: inverse,
     stdDeviation: 2
   });
-  const shifted    = yield n.feOffset({
+  const shifted    = yield f.feOffset({
     in: shadow,
     dy: 5
   });
-  const clipped    = yield n.feComposite({
+  const clipped    = yield f.feComposite({
     in:  shifted,
     in2: source,
     operator: 'in'
   });
-  const full       = yield n.feBlend({
+  const full       = yield f.feBlend({
     in:  source,
     in2: clipped,
     mode: 'multiply'
@@ -54,7 +54,7 @@ const dropShadow = source => Monad.do(function *() {
 });
 
 const program = Monad.do(function *() {
-  const source = yield n.sourceGraphic();
+  const source = yield f.sourceGraphic();
   const shadow = yield dropShadow(source);
   return Monad.of(shadow);
 });

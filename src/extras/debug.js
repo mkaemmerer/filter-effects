e7f378df935d755314ee448bd61fb13f46e8cd8d
Matt Kaemmerer <mattk@spiceworks.com>
2016-07-20 15:24:23 -0500

    Add a patterned background so user can see transparency

--- src/extras/debug.js
+++ src/extras/debug.js
@@ -6,6 +6,20 @@ const HEIGHT = 100;
 const inits = array =>
   array.map((_, i) => array.slice(0, i+1));
 
+const patternFill = {
+  nodeName:     'pattern',
+  id:           'debug-pattern-fill',
+  width:        '20',
+  height:       '20',
+  patternUnits: 'userSpaceOnUse',
+  children: [
+    { nodeName: 'rect', fill:'#DDD',  x:'0',  y:'0',  width:'10', height:'10' },
+    { nodeName: 'rect', fill:'#FFF',  x:'10', y:'0',  width:'10', height:'10' },
+    { nodeName: 'rect', fill:'#FFF',  x:'0',  y:'10', width:'10', height:'10' },
+    { nodeName: 'rect', fill:'#DDD',  x:'10', y:'10', width:'10', height:'10' }
+  ]
+};
+
 export default function build(nodes, filterAttrs = {}){
   const filters = inits(nodes)
     .map((ns, i) => ({
@@ -23,13 +37,21 @@ export default function build(nodes, filterAttrs = {}){
         height: HEIGHT,
       }]
     }));
+  const backgrounds = filters
+    .map((filter, i) => ({
+      nodeName: 'rect',
+      style:    `fill: url("#debug-pattern-fill")`,
+      x: '10',
+      y: `${10 + i * HEIGHT}`,
+      width:  WIDTH,
+      height: HEIGHT
+    }));
   const samples = filters
     .map((filter, i) => ({
       nodeName: 'image',
       'xlink:href': '../scene.svg',
       'clip-path':  `url("#debug-clip-${i}")`,
       style:        `filter: url("#debug-${i}")`,
-
       x: '10',
       y: `${10 + i * HEIGHT}`,
       width:  WIDTH,
@@ -43,8 +65,9 @@ export default function build(nodes, filterAttrs = {}){
     viewBox:  `0 0 ${(20 + WIDTH)} ${(20 + nodes.length * HEIGHT)}`,
     children: [{
         nodeName: 'defs',
-        children: [...filters, ...clipPaths]
+        children: [patternFill, ...filters, ...clipPaths]
       },
+      ...backgrounds,
       ...samples
     ]
   };

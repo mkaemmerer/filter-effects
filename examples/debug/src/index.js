02f0a6d19e4bba87ce91583280348e023bc3f754
Matt Kaemmerer <mattk@spiceworks.com>
2016-07-22 18:00:55 -0500

    Update debug example

--- examples/debug/src/index.js
+++ examples/debug/src/index.js
@@ -1,9 +1,9 @@
-import { Monad, run, create }  from 'filter-effects';
+import Filter from 'filter-effects';
 import * as f from 'filter-effects';
-import build from 'filter-effects/src/extras/debug';
+import debug from 'filter-effects/src/extras/debug';
 
 
-const crossFade = (x, y, amt) => Monad.do(function *() {
+const crossFade = (x, y, amt) => Filter.do(function *() {
   const attenuated = yield f.feColorMatrix({
     in: x,
     type: 'matrix',
@@ -18,10 +18,10 @@ const crossFade = (x, y, amt) => Monad.do(function *() {
     mode: 'normal'
   });
 
-  return Monad.of(blend);
+  return Filter.of(blend);
 });
 
-const dropShadow = source => Monad.do(function *() {
+const dropShadow = source => Filter.do(function *() {
   const inverse    = yield f.feColorMatrix({
     in: source,
     type: 'matrix',
@@ -50,22 +50,22 @@ const dropShadow = source => Monad.do(function *() {
   });
   const blend      = yield crossFade(source, full, 0.7);
 
-  return Monad.of(blend);
+  return Filter.of(blend);
 });
 
-const program = Monad.do(function *() {
+const program = Filter.do(function *() {
   const source = yield f.sourceGraphic();
   const shadow = yield dropShadow(source);
-  return Monad.of(shadow);
+  return Filter.of(shadow);
 });
 
-const filterAttrs = {
+const filter = Filter(program, {
   id:     'inner-shadow',
   x0:     '-50%',
   y0:     '-50%',
   width:  '200%',
   height: '200%'
-};
+});
 
-const svg = build(run(program), filterAttrs);
+const svg = debug(filter);
 document.body.appendChild(svg);
